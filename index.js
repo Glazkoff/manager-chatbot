bf9102ba3b502a84a5bc2483f12e3d413903b3f2
jeuub <jeuub.bodur@gmail.com>
2021-02-11 01:39:26 +0300

    logical fixes

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 /* подключение токена*/
-const token = require('./token.js')
-token() !== ""? console.log("\x1b[35m",'Token was successfully received!'):console.log("\x1b[31m",'Token was not received') 
+const token = require('./token.js');
+token() !== ""? console.log("\x1b[35m",'Token was successfully received!'):console.log("\x1b[31m",'Token was not received');
 
 /* настройка работы интервала с сервером */
 const bot = new TelegramBot(token(), {
-  polling: {
-    interval:300,
-    autoStart: true, // переключить на true на проде
-    params: {
-      timeout:10
-    }
-  }
+  polling:true
 })
 
+/* закрепить */
+function addChat(num) {
+  bot.sendMessage(num, `Если вы менеджер, отправтье в лс боту сообщение: 
+/notify ${num}`);
+}
+
+/* варианты сообщений */
 bot.on('message', msg=> {
-  bot.sendMessage(msg.chat.id, 'привет!')
+  if(msg.text.toLowerCase() == "привет, бот"){
+    bot.sendMessage(msg.chat.id, 'привет!');
+    console.log(msg)
+  }  
+});
+
+bot.onText(/\/chatID/, msg => {
+  if(parseInt(msg.chat.id)<0) {
+    addChat(msg.chat.id);
+  } 
+  else {
+    bot.sendMessage(msg.chat.id, `Переписка не является группой`);
+  }
 })
